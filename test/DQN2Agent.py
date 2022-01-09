43df2d40e94b6556033319a801479ad91bdd5c24
Sun <ffff0f0f@163.com>
2022-01-09 15:41:15 +0100

    update

--- test/DQN2Agent.py
+++ test/DQN2Agent.py
@@ -14,6 +14,51 @@ import os
 from pommerman.agents import BaseAgent
 from replay_buffer import ReplayBuffer, ReplayBuffer2
 
+class Net2(nn.Module):
+    def __init__(self,env):
+        super(Net2,self).__init__()
+        # 打包conv2d -> 黑箱，之后可直接调用
+        self.features = nn.Sequential(
+            nn.Conv2d(14,32,2,stride=1,padding=1),   # 二维卷积
+            nn.ReLU(),
+            nn.Conv2d(32,64,3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64,64,3,stride=1,padding=1),
+            nn.AdaptiveAvgPool2d(1)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(75, 32),    # 全连接层
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 6)
+        )
+
+    def forward(self, lx, ax):
+        lx = torch.FloatTensor(lx)
+        ax = torch.FloatTensor(ax)
+        #lx = lx.unsqueeze(3)
+        #print(lx[0])
+        #x = torch.unsqueeze(x, dim=0).float()
+        lx = self.features(lx)
+        lx = lx.view(lx.size(0), -1)
+        outx = torch.cat((lx, ax), 1)
+        out = self.fc(outx)
+        return out
+
+    def forward1(self, lx, ax):
+        lx = torch.FloatTensor(lx)
+        ax = torch.FloatTensor(ax)
+        lx = lx.unsqueeze(0)
+        #lx = lx.unsqueeze(3)
+        #x = torch.unsqueeze(x, dim=0).float()
+        lx = self.features(lx)
+        lx = lx.view(lx.size(0), -1)    # 重新定义矩阵的形状
+        ax = ax.unsqueeze(0)                   # 在第一维增加一个维度
+        outx = torch.cat((lx, ax), 1)       # 两个张量（tensor）拼接在一起,按列拼接
+        out = self.fc(outx)
+        action_value = out[0]
+        return action_value
 
 class DQN2Agent(BaseAgent):
     """
@@ -30,8 +75,11 @@ class DQN2Agent(BaseAgent):
         self.min_eps = args.min_eps
         self.gamma = args.gamma
         self.lr = args.lr
+        self.lr_decay = args.lr_decay
+        self.lr_decay_s = args.lr_decay_s
         self.baseAgent = SimpleAgent()
         self.episodes = args.episodes
+        self.episode = args.episode
         self.maxsteps = args.maxsteps
         self.showevery = args.showevery
 
@@ -40,24 +88,24 @@ class DQN2Agent(BaseAgent):
         # self.buffer = ReplayBuffer(self.capacity, self.batch)
         self.buffer = ReplayBuffer2(self.capacity)
         """神经网络"""
-        self.model = nn.Sequential(
-            nn.Linear(199, 128),
-            nn.ReLU(),
-            nn.Linear(128, 256),
-            nn.ReLU(),
-            nn.Linear(256, self.action_n)
-        )
-        
+        # self.model = nn.Sequential(
+        #     nn.Linear(199, 128),
+        #     nn.ReLU(),
+        #     nn.Linear(128, 256),
+        #     nn.ReLU(),
+        #     nn.Linear(256, self.action_n)
+        # )
         self.learn_step_counter = 0
+        self.memory_counter = 0
         self.eval_net, self.target_net = Net2(self.env), Net2(self.env)
         #self.optim = optim.Adam(self.model.parameters(), self.lr) #adam优化算法
         self.optim = torch.optim.Adam(self.eval_net.parameters(), lr=self.lr)
         self.MSE_loss = nn.MSELoss()
 
-    def forward(self, state):
-        state = torch.FloatTensor(state)
-        qvals = self.model(state)
-        return qvals
+    # def forward(self, state):
+    #     state = torch.FloatTensor(state)
+    #     qvals = self.model(state)
+    #     return qvals
 
     def act(self, obs, action_space):
         return self.baseAgent.act(obs,self.action_n)
@@ -67,19 +115,36 @@ class DQN2Agent(BaseAgent):
         #result = (torch.max(action, 0)[1]).numpy()
         lx = obs['local']
         ax = obs['additional']
-        action = self.eval_net.forward1(lx,ax)[0]
-        result = (torch.max(action, 0)[1]).numpy()
-        return result
+        action_value = self.eval_net.forward1(lx, ax)
+        action = (torch.max(action_value, 0)[1]).data.numpy()
+        return action
+
+    def randomact(self):
+        action = random.randrange(0, 6, 1)
+        return action
+
+
+    def choose_action(self, obs):
+        if np.random.uniform() > self.epsilon:  # greedy
+            action = self.dqnact(obs)
+            print('dqnact:', action)
+        else:
+            action = self.randomact()
+            print('randomact:', action)
+        print('eps:', self.epsilon)
+        return int(np.int64(action))
+
 
     def reward(self, featurel, featurea, action, sl, sa, rewards):
         # set up reward
-        r_wood = 0.05
+        r_wood = 0.08
         r_powerup = 0.2
-        r_put_bomb = 0.08
+        r_put_bomb = 0.05
         r_win = 0.5
-        r_fail = -1
+        r_fail = -0.5
         r_kick = 0.2
-        r_hit = -0.2
+        r_hit = -0.3
+        r_s = -0.5
 
         rigid = featurel[0].numpy()
         wood = featurel[1].numpy()
@@ -90,7 +155,7 @@ class DQN2Agent(BaseAgent):
         agent2 = featurel[6].numpy()
         agent3 = featurel[7].numpy()
         agent4 = featurel[8].numpy()
-        flame = featurel[9]
+        flame = featurel[9].numpy()
         position0 = int(featurea[0].item())
         position1 = int(featurea[1].item())
         p0 = int(sa[0].item())
@@ -109,45 +174,75 @@ class DQN2Agent(BaseAgent):
 
 
 
-        # print(agent1)
-        # print(p0, p1)
-        # print(position0, position1)
-        # print('len:', len(agent1))
-        # print('shape:', agent1.shape)
-        # print('-------------------')
-        # # reward_done
-        # # print(rewards)
 
-        if rewards == 1:
-            reward += r_win
-        if rewards == -1:
-            reward += r_fail
+        # if rewards == 1:
+        #     reward += r_win
+        # if rewards == -1:
+        #     reward += r_fail
 
-        # reward_powerup
-        sammo = int(sa[2].item())
-        if ammo > 1 and ammo > sammo:
-            reward += r_powerup
-        sstrength = int(sa[3].item())
-        if blast_strength > sstrength:
-            reward += r_powerup
-        skick = int(sa[4].item())
-        if can_kick and not skick:
-            reward += r_powerup
+        # # reward_powerup
+        # sammo = int(sa[2].item())
+        # if ammo > 1 and ammo > sammo:
+        #     reward += r_powerup
+        # sstrength = int(sa[3].item())
+        # if blast_strength > sstrength:
+        #     reward += r_powerup
+        # skick = int(sa[4].item())
+        # if can_kick and not skick:
+        #     reward += r_powerup
 
         # reward_wood
         if int(action) == 5:
-            reward += 0.01
+            reward += r_put_bomb
             bomb_flame = self.build_flame(position0, position1, rigid, blast_strength)
             num_wood = np.count_nonzero(wood*bomb_flame == 1)
             reward += num_wood*r_wood
-        # reward_kick
-        if sbomb[position0, position1] == 1 and rewards != -1:
-            reward += r_kick
-        # reward_hit_wood
+
+        # # reward_kick
+        # if sbomb[position0, position1] == 1 and rewards != -1:
+        #     reward += r_kick
+        #
+        # reward_hit
         if action>0 or action<5:
-            if (p0,p1) == (position0,position1):
+            if (p0, p1) == (position0, position1):
                 reward += r_hit
-        # reward_die
+
+
+        # reward_suicide
+        if [position0, position1] in np.argwhere(flame == 1).tolist():
+            reward += r_s
+            # print('position:', position0, position1)
+            # print('pos_falme:')
+            # print(np.argwhere(flame == 1))
+            # print('flame:')
+            # print(flame)
+
+        # reward_kill
+        # p_2 = np.argwhere(agent2 == 1).tolist()
+
+        # if len(p_4) != 0:
+        #     print('p4:', p_4[0])
+        #     print('pos_falme:')
+        #     print(np.argwhere(flame == 1).tolist())
+        # if len(p_2) != 0 and p_2[0] in np.argwhere(flame == 1).tolist():
+        #     print('pos_agent2:', p_2)
+        #     print('pos_falme:')
+        #     print(np.argwhere(flame == 1))
+        #     print('flame:')
+        #     print(flame)
+        #
+        # if np.argwhere(agent4 == 1).size != 0:
+        #     p_4 = np.argwhere(agent4 == 1).tolist()
+        #     if p_4[0] in np.argwhere(flame == 1).tolist():
+        #         print('yes')
+        #         print('pos_agent4:', p_4)
+        #         print('pos_falme:')
+        #         print(np.argwhere(flame == 1))
+        #         print('flame:')
+        #         print(flame)
+
+
+
 
         """
         exist_bomb = []
@@ -219,37 +314,45 @@ class DQN2Agent(BaseAgent):
 
         return bomb_flame
 
+    def store_transition(self, state_feature1, actions, reward, next_state_feature1, done):
+        # 存储记忆
+        self.buffer.append([state_feature1, actions[0], reward[0], next_state_feature1, done])
+        # agent3.buffer.append([state_feature3, actions[2], reward[2], next_state_feature3, done])
+        # 先走batch步之后再开始学习
+        if self.buffer.size() >= self.batch and self.episode > 50:
+            # if agent1.buffer.size() >= args.batch:
+            self.update(args.gamma, self.batch)
+        # if episode > args.tryepi and agent1.buffer.size() >= args.batch:
+        #     agent3.update(args.gamma, args.batch)
+
+
     def update(self, gamma, batch_size):
         if self.learn_step_counter % 10 == 0:
             self.target_net.load_state_dict(self.eval_net.state_dict())
         self.learn_step_counter += 1
 
         statesl, statesa, actions, rewards, next_statesl, next_statesa, done = self.buffer.sample(batch_size)
+        # self.store_transition(statesl, statesa, actions, rewards , next_statesl, next_statesa)
+
         #print(rewards)
         #print(actions)
         action_index = actions.squeeze(-2)
         #print(action_index)
         curr_Q_batch = self.eval_net(statesl, statesa)#[:,0]
         curr_Q = curr_Q_batch.gather(1, action_index.type(torch.int64)).squeeze(-1)
-        #print(curr_Q)
-        
-        next_batch = self.target_net(next_statesl, next_statesa)#[:,0]
+        # print('curr_Q', curr_Q)
+        next_batch = self.target_net(next_statesl, next_statesa).detach()   #[:,0]
         next_Q = torch.max(next_batch,1)[0]
-        #print(next_Q)
+        # print('next_Q', next_Q)
 
         #计算reward
         computed_reward = []
         for l, a, action, sl, sa, re in zip(next_statesl, next_statesa, actions, statesl, statesa, rewards):
             computed_reward.append(self.reward(l, a, action, sl, sa, re))
-
-
         #这是得到的reward
         computed_reward = torch.tensor(computed_reward)
-        
         rewards_batch = computed_reward
         #print(rewards_batch)
-
-
         # expected_Q = rewards + self.gamma * torch.max(next_Q, 1)
         expected_Q = (gamma * next_Q + rewards_batch) * ~done + done * rewards_batch
 
@@ -257,17 +360,19 @@ class DQN2Agent(BaseAgent):
         # expected_Q = done * (rewards + gamma * max_q_prime) + (1 - done) * 1 / (1 - gamma) * rewards
         # expected_Q = done * (rewards + gamma * max_q_prime) + 1 / (1 - gamma) * rewards
         loss = self.MSE_loss(curr_Q, expected_Q[0]) # TODO: try Huber Loss later too
-        # print('loss:', loss)
         self.optim.zero_grad()
         loss.backward()
         self.optim.step()
+        # print('curr_Q:')
+        # print(curr_Q)
+        # print('expected_Q')
+        # print(expected_Q)
 
     def epsdecay(self):
         self.epsilon = self.epsilon * self.eps_decay if self.epsilon > self.min_eps else self.epsilon
 
-    # def lrdecay(self,epi):
-    #     epi = self.epi
-    #     self.lr = self.lr * self.lr_decay ** (epi) if s
+    def lrdecay(self, epi):
+        self.lr = self.lr * self.lr_decay ** (epi/self.lr_decay_s)
 
     def save_model(self):
         torch.save({'dqn2Net': self.eval_net.state_dict(),'optimizer2_state_dict': self.optim.state_dict()}, 'model_dqn2.pt')
@@ -279,46 +384,3 @@ class DQN2Agent(BaseAgent):
             self.optim.load_state_dict(state_dict['optimizer2_state_dict'])
             self.target_net.load_state_dict(self.eval_net.state_dict())
 
-class Net2(nn.Module):
-    def __init__(self,env):
-        super(Net2,self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(14,32,2,stride=1,padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32,64,3,stride=1,padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64,64,3,stride=1,padding=1),
-            nn.AdaptiveAvgPool2d(1)
-        )
-        self.fc = nn.Sequential(
-            nn.Linear(75, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, 6)
-        )
-
-    def forward(self, lx, ax):
-        lx = torch.FloatTensor(lx)
-        ax = torch.FloatTensor(ax)
-        #lx = lx.unsqueeze(3)
-        #print(lx[0])
-        #x = torch.unsqueeze(x, dim=0).float()
-        lx = self.features(lx)
-        lx = lx.view(lx.size(0), -1)
-        outx = torch.cat((lx, ax), 1)
-        out = self.fc(outx)
-        return out
-
-    def forward1(self, lx, ax):
-        lx = torch.FloatTensor(lx)
-        ax = torch.FloatTensor(ax)
-        lx = lx.unsqueeze(0)
-        #lx = lx.unsqueeze(3)
-        #x = torch.unsqueeze(x, dim=0).float()
-        lx = self.features(lx)
-        lx = lx.view(lx.size(0), -1)
-        ax = ax.unsqueeze(0)
-        outx = torch.cat((lx, ax), 1)
-        out = self.fc(outx)
-        return out
--- test/multi_main.py
+++ test/multi_main.py
@@ -17,20 +17,21 @@ from plot import plot_win_rate
 def main():
     """解析参数"""
     parser = argparse.ArgumentParser(description='DQN pommerman MARL')
+    parser.add_argument('--episode', type=int, default=0, help='episode')
     parser.add_argument('--episodes', type=int, default=1000, help='episodes')
-    parser.add_argument('--maxsteps', type=int, default=500, help='maximum steps')
+    parser.add_argument('--maxsteps', type=int, default=200, help='maximum steps')
     parser.add_argument('--showevery', type=int, default=1, help='report loss every n episodes')
 
     parser.add_argument('--epsilon', type=float, default=0.9, help='parameter for epsilon greedy')
-    parser.add_argument('--eps_decay', type=float, default=0.98, help='epsilon decay rate')
+    parser.add_argument('--eps_decay', type=float, default=0.9999, help='epsilon decay rate')
     parser.add_argument('--min_eps', type=float, default=0.05, help='minimum epsilon for decaying')
     parser.add_argument('--gamma', type=float, default=0.95, help='gamma')
     parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
-    # parser.add_argument('--lr_decay', type=float, default=0.99, help='learning rate decay rate')
-    # parser.add_argument('--lr_decay_s', type=float, default=100, help='learning rate decay rate setp size')
+    parser.add_argument('--lr_decay', type=float, default=0.99, help='learning rate decay rate')
+    parser.add_argument('--lr_decay_s', type=float, default=100, help='learning rate decay rate setp size')
 
     parser.add_argument('--capacity', type=int, default=100000, help='capacity for replay buffer')
-    parser.add_argument('--batch', type=int, default=201, help='batch size for replay buffer')
+    parser.add_argument('--batch', type=int, default=128, help='batch size for replay buffer')
     parser.add_argument('--tryepi', type=int, default=100, help='episode for agent to gain experience')
     parser.add_argument('--gpu', type=str, default='0', help='gpu number')
     parser.add_argument('--win_in_epi', type=int, default='200', help='calculate win in epi..')
@@ -47,7 +48,7 @@ def main():
     agent1 = DQN2Agent(env, args)  # TODO: assertionerror; not agents.BaseAgent??
     agent2 = agents.SimpleAgent()
     # agent3 = DQN2Agent(env, args)
-    agent3 = agents.SimpleAgent()
+    agent3 = agents.RandomAgent()
     agent4 = agents.SimpleAgent()
 
     agent_list = [agent1, agent2, agent3, agent4]
@@ -55,6 +56,7 @@ def main():
 
     # plot
     list_win = []
+
 
     episode_rewards = []
     action_n = 6
@@ -73,84 +75,59 @@ def main():
 
     win_buffer = collections.deque(maxlen=args.win_in_epi)
     for episode in range(args.episodes):
+        args.episode = episode + 1
         # 固定地图
-        random.seed(2)
-        np.random.seed(2)
+        # random.seed(2)
+        # np.random.seed(2)
 
         states = env.reset()  
         # print('epi:', episode)
         done = False
         episode_reward = 0
         die = 0
+
+        # lr_decay
+        if (episode+1) % args.lr_decay_s == 0:
+            epi = episode+1
+            if episode+1 == args.episodes:
+                epi = -1
+            agent1.lrdecay(epi)
+
         for step in range(args.maxsteps):
             state_feature1 = featurize2(env, states[0])
             #state_feature3 = featurize2(env, states[2])
-            seed = random.random()
-            # print(seed)
-            # state_feature3 = featurize2(env, states[2])
+
             # 刷新环境
             # if episode % 100 == 0 and episode != 0:
             #     env.render()
-            env.render()
-            # 选择action
-            if episode < args.tryepi:       # epi < 4
-                # print("simple try")
-                actions = env.act(states)
-            else:
-                if args.epsilon > seed:
-                    actions = env.act(states)
-                else:
-                    actions = env.act(states)
-                    dqn_action1 = agent1.dqnact(state_feature1)
-                    actions[0] = int(np.int64(dqn_action1))
+            if args.episode > (args.episodes-10):
+                env.render()
+            # env.render()
 
-            print('action0:', actions[0])
-                
-            """
-            elif episode < args.ranepi:     # epi < 3000
-                # print("simple try")
-                actions = env.act(states)
-            elif episode >= args.ranepi and args.epsilon > seed:
-            # elif episode >= args.ranepi:    # epi >= 3000 a
-                #print("random try")
-                actions = env.act(states)
-                actions[0] = random.randrange(0,6,1)
-                #actions[2] = random.randrange(0,6,1)
-            elif episode >= args.ranepi and args.epsilon <= seed:  # epi >= 3000 and eps <= random.random
-                #print("dqn select")
-                actions = env.act(states)
-                dqn_action1 = agent1.dqnact(state_feature1)
-                # dqn_action3 = agent3.dqnact(state_feature3)
-                actions[0] = int(np.int64(dqn_action1))
-                #actions[2] = int(np.int64(dqn_action3))
-            """
-            
-            # actions = env.act(states)
+            # 选择action
+            actions = env.act(states)
+            actions[0] = agent1.choose_action(state_feature1)
 
             next_state, reward, done, info = env.step(actions)  # n-array with action for each agent
             if 10 not in next_state[0]['alive']:
                 info['winners'] = [1, 3]
                 reward = [-1, 1, -1, 1]
+
+            next_state_feature1 = featurize2(env, next_state[0])
+
+            if 10 not in next_state[0]['alive']:
+                info['winners'] = [1, 3]
+                reward = [-1, 1, -1, 1]
             next_state_feature1 = featurize2(env, next_state[0])
             # next_state_feature3 = featurize2(env, next_state[2])
-            #episode_reward += reward[0]
-            # 存储记忆
-            agent1.buffer.append([state_feature1, actions[0], reward[0], next_state_feature1, done])
-            # agent3.buffer.append([state_feature3, actions[2], reward[2], next_state_feature3, done])
-            # 先走batch步之后再开始学习
-            if agent1.buffer.size() >= args.batch and episode > 50:
-            # if agent1.buffer.size() >= args.batch:
-                agent1.update(args.gamma, args.batch)
-            # if episode > args.tryepi and agent1.buffer.size() >= args.batch:
-            #     agent3.update(args.gamma, args.batch)
+            # episode_reward += reward[0]
+
+            agent1.store_transition(state_feature1, actions, reward, next_state_feature1, done)
             # 更新state
             states = next_state
 
-            print('alive:', next_state[0]['alive'])
-
             if done:
                 break
-
             # agent1 die -> game over
             if 10 not in next_state[0]['alive']:
                 break
@@ -178,8 +155,6 @@ def main():
                 list_win.append(avg)
                 if len(list_win) % 500 == 0:
                     plot_win_rate(list_win)
-
-
 
         agent1.epsdecay()
         # agent3.epsdecay()
